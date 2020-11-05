eb9b463237ca57aeed3bfb2bceebf206d57606e6
Alfredo Aguirre <thefurok@gmail.com>
2020-11-05 18:11:25 -0500

    Añadimos un Swichter para que solo renderise la pagina solicitada

--- src/routes/App.js
+++ src/routes/App.js
@@ -4,16 +4,19 @@
 
 //imports necesarios
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Login from '../containers/Login';
 import Register from '../containers/Register';
 //creamos la app
 const App = () => (
   <BrowserRouter>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/register" component={Register} />
+    //Switch para que solo un elemento se renderize
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
   </BrowserRouter>
 );
 
