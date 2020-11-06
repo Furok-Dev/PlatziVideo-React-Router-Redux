1f819cf407465e473f93ab3e998d0db0876632ff
Alfredo Aguirre <thefurok@gmail.com>
2020-11-06 10:43:46 -0500

    Añadimos React-Redux para realizar el tratamiento de nuestros datos

--- src/index.js
+++ src/index.js
@@ -5,5 +5,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
+//Añadimos Redux y React-Redux
+import { Provider } from 'react-redux';
+//Logica para el Store y compartirla a la app
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
